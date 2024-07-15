055f4c058b3011e216b4a4cfa60d01be3b60d590
Aryantkd <97754312+Aryantkd@users.noreply.github.com>
2024-07-15 22:52:09 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,24 @@
+/*=============== SHOW MENU ===============*/
+const navMenu = document.getElementById('nav-menu'),
+    navToggle = document.getElementById('nav-toggle'),
+    navClose = document.getElementById('nav-close'),
+    navbacktotop = document.getElementById('nav-backtotop')
+    /* Menu show */
+if (navToggle) {
+    navToggle.addEventListener('click', () => {
+        navMenu.classList.add('show-menu')
+    })
+}
+
+/* Menu hidden */
+if (navClose) {
+    navClose.addEventListener('click', () => {
+        navMenu.classList.remove('show-menu')
+    })
+}
+if (nav - backtotop) {
+
+    function scrollToTop() {
+        window.scrollTo(0, 0);
+    }
+}
